c90879ee320d443fa07cb82d66a508b342a66efc
mat-liet <matejlietava@gmail.com>
2023-06-20 22:44:46 +0100

    FIrst commit

---
+++ poke-app/app/Http/Controllers/PokemonController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Pokemon;
+use Illuminate\Support\Facades\DB;
+
+class PokemonController extends Controller
+{
+    public function create(Request $request) {
+        $pokemon = new Pokemon();
+        $pokemon->name = $request->name;
+        $pokemon->type = $request->type;
+        $pokemon->moves = $request->moves;
+        $pokemon->save();
+        return response()->json($pokemon);
+    }
+
+    public function list(Request $request) {
+        if ($request->has('filter_name')) {
+            $pokemon = DB::table('pokemon')
+                ->where('name', 'like', "%$request->filter_name%")
+                ->get();
+            return response()->json($pokemon);
+        } else {
+            $pokemon = Pokemon::all();
+            return response()->json($pokemon);
+        }
+    }
+
+    public function find($id) {
+        if (Pokemon::where('id', $id)->exists()) {
+            $pokemon = Pokemon::find($id);
+            return response()->json($pokemon);
+        } else {
+            return response()->json([
+                'message' => "Pokemon with id $id not found."
+            ], 404);
+        }
+    }
+
+    public function update(Request $request, $id) {
+        if (Pokemon::where('id', $id)->exists()) {
+            $pokemon = Pokemon::find($id);
+            $pokemon->name = is_null($request->name) ? $pokemon->name : $request->name;
+            $pokemon->type = is_null($request->type) ? $pokemon->type : $request->type;
+            $pokemon->moves = is_null($request->moves) ? $pokemon->moves : $request->moves;
+            $pokemon->save();
+            return response()->json([
+                'message' => "Pokemon with id $id updated."
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => "Pokemon with id $id not found."
+            ], 404);
+        }
+    }
+
+    public function delete($id) {
+        if (Pokemon::where('id', $id)->exists()) {
+            Pokemon::where('id', $id)->delete();
+            return response()->json([
+                'message' => "Pokemon with id $id deleted."
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => "Pokemon with id $id not found."
+            ], 404);
+        }
+    }
+}
